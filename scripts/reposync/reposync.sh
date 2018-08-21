3d7ce8a61c29962c02d36dfddcbf26416d2ce062
Miguel P.C <mperezco@redhat.com>
2018-08-21 11:31:27 +0200

    Updated reposync

--- scripts/reposync/reposync.sh
+++ scripts/reposync/reposync.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
 DOWNLOAD_PATH='/var/www/html/repos'
-REPO_CONFIG='/root/reposync/redhat.repo'
 
+echo " --- "
+echo " Configuring repos to be synced"
+echo " --- "
+subscription-manager repos --disable='*'\
+                           --enable='rhel-7-server-rpms'\
+                           --enable='rhel-7-server-extras-rpms'\
+                           --enable='rhel-7-server-optional-rpms'\
+                           --enable='rhel-7-server-rhv-4-mgmt-agent-rpms'\
+                           --enable='rhel-7-server-source-rpms'\
+                           --enable='rhel-7-server-rh-common-rpms' 
+
+echo " --- "
+echo " Sync repos "
+echo " --- "
 reposync --download_path=${DOWNLOAD_PATH} \
-    --config=${REPO_CONFIG} \
     --download-metadata \
     --downloadcomps \
     --delete \
     --newest-only
 
 for REPODIR in $(ls ${DOWNLOAD_PATH}) ; do
-    cd ${DOWNLOAD_PATH}/$REPODIR
-    createrepo .
+    echo " --- "
+    echo " Updating {REPODIR}"
+    echo " --- "
+    cd ${DOWNLOAD_PATH}/${REPODIR}
     mv -fv productid ./repodata/
     mv -fv *.xml* ./repodata/
+    createrepo .
 done
+
+echo " --- "
+echo " Reconfiguring repos "
+echo " --- "
+subscription-manager repos --disable='*' --enable='rhel-7-server-rpms' 
+
