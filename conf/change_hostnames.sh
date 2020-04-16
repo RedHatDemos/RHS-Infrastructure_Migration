31d11ddc7a4278148a47e43013ac72f522bde25d
Nenad Peric <nenad@redhat.com>
2020-04-16 21:02:13 +0200

    change_hostnames script

---
+++ conf/change_hostnames.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+hosts=(hana oracledb tomcat)
+guid=$(hostname -s | awk -F - '{print $2}')
+
+echo ""
+echo -n "Please wait, updating hostnames with your guid: $guid ."
+
+for hostname in "${hosts[@]}"
+do 
+    echo -n "."
+    ssh root@$hostname.example.com "hostnamectl --transient set-hostname $hostname-$guid.example.com"
+    ssh root@$hostname.example.com "hostnamectl set-hostname $hostname-$guid.example.com"
+    sed -i "s/$hostname/$hostname-$guid/g" /etc/hosts
+done
+echo ""
