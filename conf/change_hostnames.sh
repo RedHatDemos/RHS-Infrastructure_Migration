228043a1e45295d888660086aa2c5168b856ec0c
Nenad Peric <nenad@redhat.com>
2020-04-17 21:50:50 +0200

    prevent rerun of change hostnames

--- conf/change_hostnames.sh
+++ conf/change_hostnames.sh
@@ -2,6 +2,10 @@
 hosts=(oracledb tomcat)
 guid=$(hostname -s | awk -F - '{print $2}')
 
+if [ -f /root/.run ]; then 
+    exit 0
+fi
+
 echo ""
 echo -n "Please wait, updating hostnames with your guid: $guid"
 
@@ -11,8 +15,12 @@ do
     ssh root@$hostname.example.com "hostnamectl --transient set-hostname $hostname-$guid.example.com" \
     && ssh root@$hostname.example.com "hostnamectl set-hostname $hostname-$guid.example.com" \
     && sed -i "s/$hostname/$hostname-$guid/g" /etc/hosts \
-    && sed -i "s/$hostname/$hostname-$guid/g" /etc/ansible/hosts
+    && sed -i "s/$hostname/$hostname-$guid/g" /etc/ansible/hosts \
+    && scp /etc/hosts root@cf.example.com:/etc/hosts
 done
 systemctl restart dnsmasq
+touch /root/.run
+ssh root@satellite.example.com "satellite-maintain service restart"
+
 echo ""
 echo "Done."
