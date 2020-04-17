8ddd655432b4351fe584b98b798128a0117451c7
Nenad Peric <nenad@redhat.com>
2020-04-17 15:18:24 +0200

    simple cond. exec of commands in change_hostnames

--- conf/change_hostnames.sh
+++ conf/change_hostnames.sh
@@ -1,16 +1,18 @@
-#!/bin/bash 
+#!/usr/bin/bash 
 hosts=(oracledb tomcat)
 guid=$(hostname -s | awk -F - '{print $2}')
 
 echo ""
-echo -n "Please wait, updating hostnames with your guid: $guid ."
+echo -n "Please wait, updating hostnames with your guid: $guid"
 
 for hostname in "${hosts[@]}"
 do 
     echo -n "."
-    ssh root@$hostname.example.com "hostnamectl --transient set-hostname $hostname-$guid.example.com"
-    ssh root@$hostname.example.com "hostnamectl set-hostname $hostname-$guid.example.com"
-    sed -i "s/$hostname/$hostname-$guid/g" /etc/hosts
-    systemctl restart dnsmasq
+    ssh root@$hostname.example.com "hostnamectl --transient set-hostname $hostname-$guid.example.com" \
+    && ssh root@$hostname.example.com "hostnamectl set-hostname $hostname-$guid.example.com" \
+    && sed -i "s/$hostname/$hostname-$guid/g" /etc/hosts \
+    && sed -i "s/$hostname/$hostname-$guid/g" /etc/ansible/hosts
 done
+systemctl restart dnsmasq
 echo ""
+echo "Done."
